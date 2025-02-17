bbc56e0f103d9df4397b20ed298a17e1ee29f163
Devon Smith <dsmith418@icloud.com>
2025-02-17 13:16:22 -0500

    Testing access token with -X POST

--- infra/exchange_token.sh
+++ infra/exchange_token.sh
@@ -20,3 +20,13 @@ FEDERATED_TOKEN="$(curl -X POST "https://sts.googleapis.com/v1/token" \
   --data "${PAYLOAD}" \
   | jq -r '.access_token'
 )"
+
+ACCESS_TOKEN="$(curl -X POST "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${SERVICE_ACCOUNT_EMAIL}:generateAccessToken" \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${FEDERATED_TOKEN}" \
+  --data '{"scope": ["https://www.googleapis.com/auth/cloud-platform"]}' \
+  | jq -r '.accessToken'
+)"
+
+echo $ACCESS_TOKEN
