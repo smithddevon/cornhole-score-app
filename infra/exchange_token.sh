1be95a3f45202482a0d641ca55c4fc1e81f597cc
Devon Smith <dsmith418@icloud.com>
2025-02-18 11:12:07 -0500

    Removed // from aud

--- infra/exchange_token.sh
+++ infra/exchange_token.sh
@@ -2,7 +2,7 @@ echo "GITLAB_OIDC_TOKEN: ${GITLAB_OIDC_TOKEN}"
 
 PAYLOAD="$(cat <<EOF
 {
-  "audience": "//iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${POOL_ID}/providers/${PROVIDER_ID}",
+  "audience": "iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${POOL_ID}/providers/${PROVIDER_ID}",
   "grantType": "urn:ietf:params:oauth:grant-type:token-exchange",
   "requestedTokenType": "urn:ietf:params:oauth:token-type:access_token",
   "scope": "https://www.googleapis.com/auth/cloud-platform",
