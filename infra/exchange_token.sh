3c811141d063a5574077463cfb8ed0b6f5b63a44
Devon Smith <dsmith418@icloud.com>
2025-02-18 14:31:14 -0500

    Added echo audience

--- infra/exchange_token.sh
+++ infra/exchange_token.sh
@@ -2,7 +2,7 @@ echo "GITLAB_OIDC_TOKEN: ${GITLAB_OIDC_TOKEN}"
 
 PAYLOAD="$(cat <<EOF
 {
-  "audience": "iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${POOL_ID}/providers/${PROVIDER_ID}",
+  "audience": "//iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${POOL_ID}/providers/${PROVIDER_ID}",
   "grantType": "urn:ietf:params:oauth:grant-type:token-exchange",
   "requestedTokenType": "urn:ietf:params:oauth:token-type:access_token",
   "scope": "https://www.googleapis.com/auth/cloud-platform",
@@ -13,6 +13,8 @@ EOF
 )"
 
 echo $PAYLOAD
+
+echo "Audience: ${PAYLOAD}" | grep audience
 
 FEDERATED_TOKEN="$(curl -v -X POST "https://sts.googleapis.com/v1/token" \
   --header "Accept: application/json" \
