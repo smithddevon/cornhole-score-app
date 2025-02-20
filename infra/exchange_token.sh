a268d3936c0e384c1bf2450c4ef33a7dbd12304f
Devon Smith <dsmith418@icloud.com>
2025-02-20 15:42:13 -0500

    Testing whole sh file

--- infra/exchange_token.sh
+++ infra/exchange_token.sh
@@ -1,9 +1,28 @@
-echo "GITLAB_OIDC_TOKEN: ${GITLAB_OIDC_TOKEN}"
+PAYLOAD="$(cat <<EOF
+{
+  "audience": "//iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${POOL_ID}/providers/${PROVIDER_ID}",
+  "grantType": "urn:ietf:params:oauth:grant-type:token-exchange",
+  "requestedTokenType": "urn:ietf:params:oauth:token-type:access_token",
+  "scope": "https://www.googleapis.com/auth/cloud-platform",
+  "subjectTokenType": "urn:ietf:params:oauth:token-type:jwt",
+  "subjectToken": "${GITLAB_OIDC_TOKEN}"
+}
+EOF
+)"
 
-RESPONSE=$(curl -v -X POST "https://sts.googleapis.com/v1/token" \
+FEDERATED_TOKEN="$(curl -v -X POST "https://sts.googleapis.com/v1/token" \
   --header "Accept: application/json" \
   --header "Content-Type: application/json" \
   --data "${PAYLOAD}" \
-)
+  | jq -r '.access_token'
+)"
 
-echo "Response: ${RESPONSE}"
+ACCESS_TOKEN="$(curl -v -X POST "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${SERVICE_ACCOUNT_EMAIL}:generateAccessToken" \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${FEDERATED_TOKEN}" \
+  --data '{"scope": ["https://www.googleapis.com/auth/cloud-platform"]}' \
+  | jq -r '.accessToken'
+)"
+
+echo $ACCESS_TOKEN
