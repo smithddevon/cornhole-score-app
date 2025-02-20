e097f3d59552db6584b4a5df75470bfaf220e043
Devon Smith <dsmith418@icloud.com>
2025-02-20 15:11:52 -0500

    Debugging gitlab token

--- infra/exchange_token.sh
+++ infra/exchange_token.sh
@@ -1,24 +1,9 @@
-echo "GITLAB_OIDC_TOKEN: ${GITLAB_OIDC_TOKEN}" > token_value.txt
+echo "GITLAB_OIDC_TOKEN: ${GITLAB_OIDC_TOKEN}"
 
-PAYLOAD="$(cat <<EOF
-{
-  "audience": "//iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${POOL_ID}/providers/${PROVIDER_ID}",
-  "grantType": "urn:ietf:params:oauth:grant-type:token-exchange",
-  "requestedTokenType": "urn:ietf:params:oauth:token-type:access_token",
-  "scope": "https://www.googleapis.com/auth/cloud-platform",
-  "subjectTokenType": "urn:ietf:params:oauth:token-type:jwt",
-  "subjectToken": "${GITLAB_OIDC_TOKEN}"
-}
-EOF
-)"
-
-echo $PAYLOAD
-
-FEDERATED_TOKEN="$(curl -v -X POST "https://sts.googleapis.com/v1/token" \
+RESPONSE=$(curl -v -X POST "https://sts.googleapis.com/v1/token" \
   --header "Accept: application/json" \
   --header "Content-Type: application/json" \
   --data "${PAYLOAD}" \
-  | jq -r '.access_token'
-)"
+)
 
-echo $FEDERATED_TOKEN
+echo "Response: ${RESPONSE}"
