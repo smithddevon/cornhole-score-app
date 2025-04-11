bd00642323a98718d48b8afc6bf2b88d0911523d
Devon Smith <dsmith418@icloud.com>
2025-04-11 13:49:08 -0400

    Testing access token script

--- infra/exchange_token.sh
+++ infra/exchange_token.sh
@@ -21,13 +21,10 @@ FEDERATED_TOKEN="$(curl -v -X POST "https://sts.googleapis.com/v1/token" \
 
 echo $FEDERATED_TOKEN
 
-
-# Testing Access Token 
-ACCESS_TOKEN_RESPONSE=$(curl -v -X POST "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${SERVICE_ACCOUNT_EMAIL}:generateAccessToken" \
+ACCESS_TOKEN="$(curl -v -X POST "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${SERVICE_ACCOUNT_EMAIL}:generateAccessToken" \
   --header "Accept: application/json" \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${FEDERATED_TOKEN}" \
-  --data '{"scope": ["https://www.googleapis.com/auth/cloud-platform"]}' 2>&1)
-
-# Print the full response
-echo "Access Token Response: ${ACCESS_TOKEN_RESPONSE}"
+  --data '{"scope": ["https://www.googleapis.com/auth/cloud-platform"]}' \
+  | jq -r '.accessToken'
+)"
